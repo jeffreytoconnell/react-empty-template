fead42547b5b1b2d2df38e6343f401ca45c379f8
jeffreytoconnell <jeffreytoconnell@gmail.com>
2017-09-09 18:59:02 -0700

    updated

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
 // INDEX.JS
+
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+
+// import SomeClass from './compontents/SomeFile';
+
+ReactDOM.render(
+    <SomeClass />,
+    document.getElementById('root')
+);
